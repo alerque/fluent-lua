3a6b43fe13f09e96e20461037cc1059a7139535e
Caleb Maclennan <caleb@alerque.com>
2019-09-12 17:28:45 +0300

    Add more tests to make sure objects keep their distance

--- spec/fluent_spec.lua
+++ spec/fluent_spec.lua
@@ -4,31 +4,45 @@ describe('fluent', function ()
 
   it('should instantiate with a locale', function ()
     local locale = "en-US"
-    local bundle = fluent(locale)
-    assert.is.truthy(bundle:is_a(fluent))
-    assert.same(locale, bundle.locale)
+    local en = fluent(locale)
+    assert.truthy(en:is_a(fluent))
+    assert.same(locale, en.locale)
   end)
 
   it('should parse single simple messages', function ()
-    local locale = "en-US"
-    local bundle = fluent(locale)
-    bundle:add_messages("foo = bar")
-    assert.same("bar", bundle.messages.foo)
+    local en = fluent("en-US")
+    en:add_messages("foo = bar")
+    assert.same("bar", en.messages.foo)
   end)
 
   it('should parse multiple simple messages', function ()
-    local locale = "en-US"
-    local bundle = fluent(locale)
-    bundle:add_messages("foo = bar\nbar = baz")
-    assert.same("bar", bundle.messages.foo)
-    assert.same("baz", bundle.messages.bar)
+    local en = fluent("en-US")
+    en:add_messages("foo = bar\nbar = baz")
+    assert.same("bar", en.messages.foo)
+    assert.same("baz", en.messages.bar)
   end)
 
   it('should return formatted strings', function ()
-    local locale = "en-US"
-    local bundle = fluent(locale)
-    bundle:add_messages("foo = bar")
-    assert.same("bar", bundle:format("foo"))
+    local en = fluent("en-US")
+    en:add_messages("foo = bar")
+    assert.same("bar", en:format("foo"))
+  end)
+
+  it('should keep locale instances separate', function ()
+    local en = fluent("en-US")
+    local tr = fluent("tr-TR")
+    assert.not_same(en, tr)
+  end)
+
+  it('should return locale specific strings', function ()
+    local en = fluent("en-US")
+    local tr = fluent("tr-TR")
+    en:add_messages("foo = bar")
+    tr:add_messages("foo = baz")
+    assert.same("bar", en:format("foo"))
+    assert.same("baz", tr:format("foo"))
   end)
 
 end)
+
+
