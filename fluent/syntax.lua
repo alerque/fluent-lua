bd69cee03afc9c24cd975625cdecb2d78d95c411
Caleb Maclennan <caleb@alerque.com>
2019-09-13 20:11:10 +0300

    Define parser based on EBNF: junk is the hardest...

--- fluent/syntax.lua
+++ fluent/syntax.lua
@@ -5,18 +5,18 @@ local epnf = require("epnf")
 -- luacheck: push ignore
 local ftlparser = epnf.define(function (_ENV)
   local blank_inline = P" "^1
-  local line_end = P"\r\n" + P"\n"-- + -1
+  local line_end = P"\r\n" + P"\n"
   local blank_block = (blank_inline^-1 * line_end)^1
   local blank = (blank_inline + line_end)^1
   local digits = R"09"^1
-  local junk_line = -P"\n"^0 * P"\n"-- * -1
-  local comment_char = 1 - line_end
+  local junk_line =  (1-line_end)^0 * line_end
   Junk = junk_line * (junk_line - P"#" - P"-" - R("az","AZ"))^0
-  CommentLine = (P"###" + P"##" + P"#") * (" " * comment_char^0) * line_end
+  local comment_char = 1 - line_end
+  CommentLine = (P"###" + P"##" + P"#") * (" " * comment_char^0)-1 * line_end
   Term = P"xxx"
   Message = P"foo = bar"
   Entry = (V"Message" * line_end) + (V"Term" * line_end) + V"CommentLine"
-  Resource = (V"Entry" + blank_block + V"Junk")^0 * EOF"Unparsable input"
+  Resource = (V"Entry" + blank_block + V"Junk")^0 * EOF"unparsable input"
   START("Resource")
 end)
 -- luacheck: pop
