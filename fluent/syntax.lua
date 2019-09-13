b11ee644c654b1e67b6cd723f2b21cd0fe3742c0
Caleb Maclennan <caleb@alerque.com>
2019-09-13 16:46:26 +0300

    Check that parser input is always and only a string

--- fluent/syntax.lua
+++ fluent/syntax.lua
@@ -5,13 +5,19 @@ local epnf = require("epnf")
 -- luacheck: push ignore
 local ftlparser = epnf.define(function (_ENV)
   START("Resource")
-  Resource = S"-"
+  Resource = P""
 end)
 -- luacheck: pop
 
 local FluentSyntax = class({
     parser = ftlparser,
     parse = function (self, input)
+      if not self or type(self) ~= "table" then
+        error("FluentSyntax.parser error: must be invoked as a method")
+      end
+      if not input or type(input) ~= "string" then
+        error("FluentSyntax.parser error: input must be a string")
+      end
       local ast = epnf.parsestring(self.parser, input)
       return ast
     end
--- spec/fluent_syntax_spec.lua
+++ spec/fluent_syntax_spec.lua
@@ -9,8 +9,20 @@ describe('fluent.syntax', function ()
 
   describe('parser', function ()
 
+    it('should be called as a method', function ()
+      assert.error(function () syntax.parse() end)
+      assert.error(function () syntax.parse("") end)
+    end)
+
+    it('should require a string', function ()
+      assert.error(function () syntax:parse() end)
+      assert.error(function () syntax:parse(false) end)
+      assert.error(function () syntax:parse(1) end)
+      assert.error(function () syntax:parse({}) end)
+    end)
+
     it('should return an AST', function ()
-      local ast = syntax:parse("-")
+      local ast = syntax:parse("")
       assert.same("Resource", ast.id)
     end)
 
