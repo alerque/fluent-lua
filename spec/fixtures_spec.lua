ffad5aaf1e80a270eaf9b42cbe3c99c678c14a2e
Caleb Maclennan <caleb@alerque.com>
2019-09-22 10:38:42 +0300

    Add busted tests for structure fixtures

--- spec/fixtures_spec.lua
+++ spec/fixtures_spec.lua
@@ -12,10 +12,46 @@ local function filetostring (fname)
   return content
 end
 
-describe('upstream fixture', function ()
+describe('upstream reference fixture', function ()
   local syntax = FluentSyntax()
 
   local fixtures_dir = lfs.currentdir() .. "/spec/fixtures/"
+  for object in lfs.dir(fixtures_dir) do
+    local fname = fixtures_dir .. object
+    if fname:match(".ftl$") then
+      describe(object, function ()
+        local ftl = filetostring(fname)
+        local jsonref = filetostring(fname:gsub(".ftl$", ".json"))
+        local reference = json.decode(jsonref)
+
+        local ast
+
+        it('should have a reference', function()
+          assert.equal("table", type(reference))
+        end)
+
+        it('should parse without blowing up', function ()
+          assert.no.error(function () ast = syntax:parse(ftl) end)
+        end)
+
+        it('should have a Reference as the AST root', function ()
+          assert.equal("Resource", ast.type)
+        end)
+
+        it('should matach the referece result', function ()
+          assert.same(reference, ast)
+        end)
+
+      end)
+    end
+  end
+
+end)
+
+describe('upstream structure fixture', function ()
+  local syntax = FluentSyntax()
+
+  local fixtures_dir = lfs.currentdir() .. "/spec/structure/"
   for object in lfs.dir(fixtures_dir) do
     local fname = fixtures_dir .. object
     if fname:match(".ftl$") then
