110c7f69ccbf48851ec9e9fb48c9c225af117250
Caleb Maclennan <caleb@alerque.com>
2022-03-10 17:06:12 +0300

    Fix bundle/locale instance issolation

--- fluent/init.lua
+++ fluent/init.lua
@@ -8,10 +8,9 @@ local FluentSyntax = require("fluent.syntax")
 local FluentResource = require("fluent.resource")
 
 local FluentBundle = class()
-FluentBundle.locales = {}
-FluentBundle.locale = "und"
 
 function FluentBundle:_init (locale)
+  self.locales = {}
   self:set_locale(locale)
   -- self:catch(self.get_message)
   return self
@@ -22,6 +21,7 @@ function FluentBundle:set_locale (locale)
   if not self.locales[self.locale] then
     self.locales[self.locale] = FluentResource()
   end
+  return self:get_locale()
 end
 
 function FluentBundle:get_locale ()
@@ -30,18 +30,17 @@ end
 
 function FluentBundle:get_resource (locale)
   local locales = self.locales
-  local locale = self:get_locale(locale)
-  return locales[locale]
+  return locales[locale or self:get_locale()]
 end
 
 function FluentBundle:get_message (identifier)
   local resource = self:get_resource()
   -- TODO iterate over fallback locales if not found in current one
-  return resource:get_message(identifier) or nil
+  return resource:get_message(identifier)
 end
 
 function FluentBundle:add_messages (input, locale)
-  if locale then self:set_locale(locale) end
+  locale = locale or self:get_locale()
   local syntax = FluentSyntax()
   local messages =
     type(input) == "string"
@@ -49,12 +48,12 @@ function FluentBundle:add_messages (input, locale)
     or tablex.reduce('+', tablex.imap(function (v)
         return syntax:parsestring(v)
       end, input))
-  self.locales[self.locale]:__add(messages)
-  return self
+  local resource = self:get_resource(locale)
+  return resource + messages
 end
 
 function FluentBundle:format (identifier, parameters)
-  local resource = self.locales[self.locale]
+  local resource = self:get_resource()
   local message = resource:get_message(identifier)
   return message:format(parameters)
 end
