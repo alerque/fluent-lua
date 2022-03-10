208ee6e78ca8dfb804461a7f08f4a9f9d82fed07
Caleb Maclennan <caleb@alerque.com>
2022-03-10 21:08:14 +0300

    Activate and expand on resource property accessor tests

--- spec/fluent_spec.lua
+++ spec/fluent_spec.lua
@@ -96,15 +96,16 @@ foo =
   end)
 
   describe('messages', function ()
-    local bundle
+    local bundle, en
 
     before_each(function ()
       bundle = FluentBundle("en")
+      en = bundle:get_resource()
       bundle:add_messages("hi = Hello { $name }!\nfoo = bar\nbar = baz\n    .bax = qux")
     end)
 
     after_each(function ()
-      bundle = nil
+      bundle, en = nil, nil
     end)
 
     it('can be accessed with getter', function ()
@@ -116,10 +117,13 @@ foo =
 
     it('can be accessed as properties', function ()
       assert.same("bar", bundle.foo:format())
+      assert.same("bar", en.foo:format())
       assert.same("bar", bundle["foo"]:format())
       assert.same("baz", bundle.bar:format())
+      assert.same("baz", en.bar:format())
       assert.same("baz", bundle["bar"]:format())
       assert.error(function () return bundle.apple:format() end)
+      assert.error(function () return en.apple:format() end)
       assert.error(function () return bundle["apple"]:format() end)
     end)
 
@@ -151,6 +155,7 @@ foo =
 
     it('preserves attributes when messages are added', function ()
       assert.same("baz", bundle.bar())
+      assert.same("baz", en.bar())
       assert.same("baz", bundle:format("bar"))
       assert.same("qux", bundle:get_message("bar"):get_attribute("bax")())
       -- assert.same("qux", bundle["bar.bax"]())
@@ -159,35 +164,52 @@ foo =
       -- assert.same("qux", bundle["bar.bax"]())
       -- assert.same("qux", bundle.bar:get_attribute("bax")())
       assert.same("baz", bundle.bar())
+      -- TODO fix property accessor on resource
+      -- assert.same("baz", en.bar())
       assert.same("baz", bundle:format("bar"))
       assert.same("qux", bundle:get_message("bar"):get_attribute("bax")())
       bundle:add_messages("bar = rebar")
       assert.same("rebar", bundle.bar())
+      -- TODO fix property accessor on resource
+      -- assert.same("rebar", en.bar())
       assert.same("rebar", bundle:format("bar"))
       assert.error(function() return bundle:get_message("bar"):get_attribute("bax")() end)
       -- assert.same("qux", bundle.bar.bax())
     end)
 
-    pending('can be called', function ()
+    it('can be called', function ()
       assert.same("bar", bundle.foo())
+      assert.same("bar", en.foo())
       assert.same("bar", bundle["foo"]())
+      assert.same("bar", en["foo"]())
       assert.same("baz", bundle.bar())
+      assert.same("baz", en.bar())
       assert.same("baz", bundle["bar"]())
+      assert.same("baz", en["bar"]())
     end)
 
-    pending('can be called with parameters', function ()
+    it('can be called with parameters', function ()
       assert.same("Hello World!", bundle.hi({name = "World"}))
+      assert.same("Hello World!", en.hi({name = "World"}))
       assert.same("Hello World!", bundle["hi"]({name = "World"}))
+      assert.same("Hello World!", en["hi"]({name = "World"}))
       assert.same("Hello World!", bundle["hi"]:format({name = "World"}))
+      assert.same("Hello World!", en["hi"]:format({name = "World"}))
     end)
 
-    pending('can be cast to strings', function ()
+    it('can be cast to strings', function ()
       assert.same("baz", tostring(bundle.bar))
+      assert.same("baz", tostring(en.bar))
       assert.same("bar", tostring(bundle["foo"]))
+      assert.same("bar", tostring(en["foo"]))
       assert.same("xbar", "x" .. bundle.foo)
+      assert.same("xbar", "x" .. en.foo)
       assert.same("xbaz", "x" .. bundle["bar"])
+      assert.same("xbaz", "x" .. en["bar"])
       assert.same("barx", bundle.foo .. "x")
+      assert.same("barx", en.foo .. "x")
       assert.same("bazx", bundle["bar"] .. "x")
+      assert.same("bazx", en["bar"] .. "x")
     end)
 
   end)
